593e5c56f3cd062c28099d87442582b0a7d690d2
Dan <daniel@mail-richter.net>
2023-09-07 13:15:03 +0200

    changed ports

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '<a string of random characters>')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == "True"
 
-ALLOWED_HOSTS = [os.environ.get('DOMAIN'),]
+ALLOWED_HOSTS = ['.easy-platform.business',]
 if DEBUG:
     ALLOWED_HOSTS = ["*",]
 
