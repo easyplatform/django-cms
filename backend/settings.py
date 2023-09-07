a52554a97670899e0ce4a48b308ad617f1100c13
Dan <daniel@mail-richter.net>
2023-09-07 14:12:19 +0200

    removed bug

--- backend/settings.py
+++ backend/settings.py
@@ -199,7 +199,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 # DEFAULT_FILE_STORAGE is configured using DEFAULT_STORAGE_DSN
